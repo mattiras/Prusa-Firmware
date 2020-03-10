4a997e973a19e50b2926d69e6e1502a6b23853b6
mattiras <matti_rasanen@hotmail.fi>
2020-03-10 13:02:51 +0200

    Update 1_75mm_MK2-RAMBo13a-E3Dv6full.h
    Simple change to make "Bear" extruder and x-axis to work in Prusa i3 MK2(S)
    https://github.com/gregsaun/bear_extruder_and_x_axis

--- Firmware/variants/1_75mm_MK2-RAMBo13a-E3Dv6full.h
+++ Firmware/variants/1_75mm_MK2-RAMBo13a-E3Dv6full.h
@@ -230,7 +230,7 @@ BED SETTINGS
 #define MESH_HOME_Z_SEARCH 5 //Z lift for homing, mesh bed leveling etc.
 
 #define X_PROBE_OFFSET_FROM_EXTRUDER 23     // Z probe to nozzle X offset: -left  +right
-#define Y_PROBE_OFFSET_FROM_EXTRUDER 9     // Z probe to nozzle Y offset: -front +behind
+#define Y_PROBE_OFFSET_FROM_EXTRUDER 4     // Z probe to nozzle Y offset: -front +behind
 #define Z_PROBE_OFFSET_FROM_EXTRUDER -0.4  // Z probe to nozzle Z offset: -below (always!)
 #endif
 
